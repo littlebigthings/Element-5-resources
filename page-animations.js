41cffe99b4f11a6f968a31a4b0e71688859175f0
Himanshu Raj <himanshurajverma549@gmail.com>
2022-05-16 16:32:01 +0530

    final changes after review

--- page-animations.js
+++ page-animations.js
@@ -1,4 +1,3 @@
-// import ScrollSmoother from './ScrollSmoother.js'
 function animateText(){
     let textWrp = document.querySelector(".hero-main-title");
     let spanTxt = textWrp != undefined && textWrp.querySelector(".span-48");
@@ -19,9 +18,6 @@ function animateText(){
 function animateSections(){
     gsap.registerPlugin(ScrollTrigger);
     let allSections = document.querySelectorAll("[data-animate='section']");
-    let leftCols = document.querySelectorAll("[data-animate='col-left']");
-    let rightCol = document.querySelectorAll("[data-animate='col-right']");
-    console.log(leftCols, rightCol)
     allSections.forEach(section => {
         gsap.set(section,{opacity:0, y:"-20px", scale:0.8})
         ScrollTrigger.create({
@@ -33,38 +29,6 @@ function animateSections(){
                     opacity:1,
                     scale:1,
                     y:"0px",
-                    duration:0.5,
-                    ease: "circ.out",
-                })
-                self.disable();
-            }
-        })
-    })
-    leftCols.forEach((column, index) => {
-        gsap.set(column,{opacity:0, x:"-100px",});
-        gsap.set(rightCol[index],{opacity:0, x:"100px",});
-        ScrollTrigger.create({
-            trigger: column,
-            // markers: true,
-            start: "top 50%",
-            onEnter: self => {
-                gsap.to(column,{
-                    opacity:1,
-                    x:"0px",
-                    duration:0.5,
-                    ease: "circ.out",
-                })
-                self.disable();
-            }
-        })
-        ScrollTrigger.create({
-            trigger: rightCol[index],
-            // markers: true,
-            start: "top 50%",
-            onEnter: self => {
-                gsap.to(rightCol[index],{
-                    opacity:1,
-                    x:"0px",
                     duration:0.5,
                     ease: "circ.out",
                 })
