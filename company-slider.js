21bd3d4eb59d6a7bc31d9c23557e1e64112abeac
Himanshu Raj <himanshurajverma549@gmail.com>
2022-05-18 19:47:24 +0530

    up for final review

--- company-slider.js
+++ company-slider.js
@@ -1,16 +1,35 @@
 function addStoryCarousel() {
     let carouselComp = document.querySelector(".slider-list-wrapper");
+    let lineComponent = document.querySelector("[data-slider='lines']");
     let prevBtn = document.querySelector(".prev-arrow-btn");
     let nextBtn = document.querySelector(".next-arrow-btn");
-    let lineArr = [...document.querySelectorAll("[data-line]")];
-    let runOppo = false;
     if (carouselComp != undefined && prevBtn != undefined && nextBtn != undefined) {
         $(carouselComp).on("init", (event, slick) => {
             if (slick.currentSlide == 0) {
                 prevBtn.style.opacity = "20%";
             }
+            if (lineComponent != undefined) {
+                let image = lineComponent.querySelector(".image-lines");
+                for (let i = 1; i < slick.slideCount; i++) {
+                    lineComponent.appendChild(image.cloneNode(true))
+                }
+            }
         });
         let slider = $(carouselComp).slick({
+            dots: false,
+            slidesToScroll: 1,
+            infinite: false,
+            autoplay: false,
+            variableWidth: true,
+            centerMode: false,
+            draggable: false,
+            focusOnSelect: false,
+            arrows: true,
+            prevArrow: prevBtn,
+            nextArrow: nextBtn,
+            asNavFor: lineComponent,
+        });
+        let lineSlider = $(lineComponent).slick({
             dots: false,
             slidesToScroll: 1,
             infinite: false,
@@ -20,17 +39,10 @@ function addStoryCarousel() {
             centerMode: false,
             draggable: false,
             focusOnSelect: false,
-            arrows: true,
-            prevArrow: prevBtn,
-            nextArrow: nextBtn,
+            asNavFor: carouselComp,
         });
         slider.on("beforeChange", (event, slick, currentSlide, nextSlide) => {
             if (nextSlide >= 1) {
-                gsap.to(lineArr[0], {
-                    opacity: 1,
-                    duration: 0.5,
-                    ease: 'linear',
-                })
                 prevBtn.style.opacity = "100%";
             }
             if (nextSlide == slick.slideCount - 1) {
@@ -41,25 +53,6 @@ function addStoryCarousel() {
             }
             if (nextSlide == 0) {
                 prevBtn.style.opacity = "10%"
-            }
-            if (currentSlide < nextSlide) {
-                let lineToAnim = lineArr.find(elm => parseInt(elm.getAttribute('data-line')) === nextSlide);
-                if (lineToAnim != undefined) {
-                    gsap.to(lineToAnim, {
-                        opacity: 1,
-                        duration: 0.5,
-                        ease: 'linear',
-                    })
-                }
-            } else {
-                let lineToAnim = lineArr.find(elm => parseInt(elm.getAttribute('data-line')) === currentSlide);
-                if (lineToAnim != undefined) {
-                    gsap.to(lineToAnim, {
-                        opacity: 0.2,
-                        duration: 0.5,
-                        ease: 'linear',
-                    })
-                }
             }
         })
     }
