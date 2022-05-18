184aac747f2269201c1cee0b6123e5069acce15c
Himanshu Raj <himanshurajverma549@gmail.com>
2022-05-18 17:43:26 +0530

    svg anim up for review

--- company-slider.js
+++ company-slider.js
@@ -2,7 +2,8 @@ function addStoryCarousel() {
     let carouselComp = document.querySelector(".slider-list-wrapper");
     let prevBtn = document.querySelector(".prev-arrow-btn");
     let nextBtn = document.querySelector(".next-arrow-btn");
-
+    let lineArr = [...document.querySelectorAll("[data-line]")];
+    let runOppo = false;
     if (carouselComp != undefined && prevBtn != undefined && nextBtn != undefined) {
         $(carouselComp).on("init", (event, slick) => {
             if (slick.currentSlide == 0) {
@@ -25,6 +26,11 @@ function addStoryCarousel() {
         });
         slider.on("beforeChange", (event, slick, currentSlide, nextSlide) => {
             if (nextSlide >= 1) {
+                gsap.to(lineArr[0], {
+                    opacity: 1,
+                    duration: 0.5,
+                    ease: 'linear',
+                })
                 prevBtn.style.opacity = "100%";
             }
             if (nextSlide == slick.slideCount - 1) {
@@ -35,6 +41,25 @@ function addStoryCarousel() {
             }
             if (nextSlide == 0) {
                 prevBtn.style.opacity = "10%"
+            }
+            if (currentSlide < nextSlide) {
+                let lineToAnim = lineArr.find(elm => parseInt(elm.getAttribute('data-line')) === nextSlide);
+                if (lineToAnim != undefined) {
+                    gsap.to(lineToAnim, {
+                        opacity: 1,
+                        duration: 0.5,
+                        ease: 'linear',
+                    })
+                }
+            } else {
+                let lineToAnim = lineArr.find(elm => parseInt(elm.getAttribute('data-line')) === currentSlide);
+                if (lineToAnim != undefined) {
+                    gsap.to(lineToAnim, {
+                        opacity: 0.2,
+                        duration: 0.5,
+                        ease: 'linear',
+                    })
+                }
             }
         })
     }
